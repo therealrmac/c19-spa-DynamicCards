1af2e7187920e4c2dc238529dfa8f036f9f40b99
Nashville Software School <nashvillesoftwareschool@Nashvilles-MacBook-Pro.local>
2017-04-21 15:42:03 -0500

    added the functionality and some css

---
+++ README.md
@@ -0,0 +1,8 @@
+# Dyanimc Cards event Listener
+
+** It's basically done but I think I might have some more to do **
+
+### Instruction
+1. type any text into the textArea 
+2. press the create button to crate a new card with the text of your textArea
+3. press the delete button to remove the card.
--- cards.js
+++ cards.js
@@ -1,1 +1,27 @@
 console.log("Hello");
+var textArea= document.getElementById('inputArea');
+var button= document.getElementById('create');
+var newCardArea= document.getElementById('newCardArea');
+var deleteCard=document.getElementsByClassName('delCard');
+
+
+button.addEventListener('click', () =>{
+	var x= textArea.value;
+	console.log(x);
+	var newCard= `<div class="card">
+					<article class="delCard">${x}</article>
+					<button class="delete">Delete</button>
+					</div>`;
+	newCardArea.innerHTML+= newCard;	
+	deleteButton();
+});
+
+function deleteButton(){
+	var delButton= document.getElementsByClassName('delete');
+	for (var i=0; i< delButton.length; i++){
+		delButton[i].addEventListener('click', function (){
+			this.parentNode.remove();
+		});
+	}
+	textArea.value="";
+}
